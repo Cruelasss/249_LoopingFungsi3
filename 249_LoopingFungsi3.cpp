3d14a33bf601c464adc9526d5fb0cb9b1a97b72f
Cruelasss <162581217+Cruelasss@users.noreply.github.com>
2024-07-10 19:13:51 +0700

    Update 249_LoopingFungsi3.cpp

--- 249_LoopingFungsi3.cpp
+++ 249_LoopingFungsi3.cpp
@@ -3,16 +3,25 @@ using namespace std;
 
 int main()
 {
-    string pilihan;
-    do 
-   {
-    cout << "makan " << endl;
-    cout << "main game" << endl;
-    cout << "Ibadah" << endl;
+    int i;
 
-    cout << "Apakah anda masih hidup ? (Yes/No)";
-    cin >> pilihan;
+    string nama(5); 
 
-   }while (pilihan == "Yes "); 
+    for(i = 0; i < 7; i++);(
+        cout << i << "." << "AKMAL" << endl;
+    )
+
+    for(i=0; i < 5; i++);(
+        cout <<"Masukkan nama =";
+        cin >> nama(i);
+    )
+
+    COUT << endl;
+    cout << "--------------------" << endl;
+    cout << "KUMPULAN NAMA-NAMA" << endl;
+
+    for(i=0; i<5; i++);(
+        cout << "Namanya adalah" << nama(i) << endl;
+    )
 }
     
